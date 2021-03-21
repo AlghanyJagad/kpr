fb99f54a1fd7d04d07af20500d53da3caf97438a
AlghanyJagad <58999743+AlghanyJagad@users.noreply.github.com>
2021-03-21 20:45:18 +0700

    chart etc

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -36,6 +36,7 @@
     <link id="color" rel="stylesheet" href="{{ asset('assets/css/color-1.css') }}" media="screen">
     <!-- Responsive css-->
     <link rel="stylesheet" type="text/css" href="{{ asset('assets/css/responsive.css') }}">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.6.0/Chart.bundle.js" charset="utf-8"></script>
 
     @stack('style')
 </head>
